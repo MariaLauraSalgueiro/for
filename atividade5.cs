a3ce45c69ea865a4a57f45c0e817612b1ad86e97
MariaLauraSalgueiro <salgueiromarialaura34@gmail.com>
2026-05-19 16:16:45 -0300

    "fix: adicionando a explicação do mais organizado"
    Added explanation comparing 'for' and 'while' loops.

--- atividade5.cs
+++ atividade5.cs
@@ -4,9 +4,16 @@ Escolha uma das atividades anteriores e resolva de duas formas:
 
 - Uma usando `for`;
 - Outra usando `while`.
-
+/*
+/*
 Depois, escreva uma breve explicação dizendo qual das duas formas ficou mais organizada.*/
-
+O 'fo'r ficou mais organizado porque é uma estrutura de repetição que concentra a inicialização, onde a variável é criada,
+a condição de parada e o incremento em uma única linha, tornando o código mais compacto e fácil de entender.
+Já no 'while' e, esses três elementos ficam espalhados pelo 'código' (a inicialização fica antes 'do' laço 
+e o incremento fica solto dentro 'do' bloco). Isso deixa o código mais longo e aumenta o risco de 
+esquecermos o contador++,o que levaria a um loop "infinito".
+*/
+    
 public static class atividade5
 {
     public static void executar()
